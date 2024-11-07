dfe05b9f34bf7e664cd0c9bc2cc4c5fb1bf7087a
authzedbot <infrastructure@authzed.com>
2024-11-07 22:25:54 +0100

    Brew formula update for zed version v0.23.0

--- Formula/zed.rb
+++ Formula/zed.rb
@@ -5,15 +5,15 @@
 class Zed < Formula
   desc "command-line client for managing SpiceDB"
   homepage "https://github.com/authzed/zed"
-  version "0.22.0"
+  version "0.23.0"
   license "Apache-2.0"
 
   depends_on "go" => :build
 
   on_macos do
     on_intel do
-      url "https://github.com/authzed/zed/releases/download/v0.22.0/zed_0.22.0_darwin_amd64.tar.gz"
-      sha256 "d54dcbea293e03ba9e9b1f30029d0361bf7bc5a7c6b6ef3950a4b0d2e60a9d0c"
+      url "https://github.com/authzed/zed/releases/download/v0.23.0/zed_0.23.0_darwin_amd64.tar.gz"
+      sha256 "1c1b5291f9567d8c8d6182093d9a69474c05c5f85f71885675de572e2f03b738"
 
       def install
         if build.head?
@@ -26,8 +26,8 @@ class Zed < Formula
       end
     end
     on_arm do
-      url "https://github.com/authzed/zed/releases/download/v0.22.0/zed_0.22.0_darwin_arm64.tar.gz"
-      sha256 "961fbb44f09c9056e156030cdb0aab4c5a7b5ab013986be7b8d6d776d7e6eb0e"
+      url "https://github.com/authzed/zed/releases/download/v0.23.0/zed_0.23.0_darwin_arm64.tar.gz"
+      sha256 "5653d39a2efa113a5e453ad719c465a680e47f58255d3de99ab0ea66609c550b"
 
       def install
         if build.head?
@@ -44,8 +44,8 @@ class Zed < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/authzed/zed/releases/download/v0.22.0/zed_0.22.0_linux_amd64_gnu.tar.gz"
-        sha256 "d0cec51f1e9a96f5d8f285c568f670b53b13d3fbaca8a98602ad216ecbd3f85f"
+        url "https://github.com/authzed/zed/releases/download/v0.23.0/zed_0.23.0_linux_amd64_gnu.tar.gz"
+        sha256 "8dafa3382bfc36b8fea4330624a1027cc4dac49382527ba92f23e94843ab4b36"
 
         def install
           if build.head?
@@ -60,8 +60,8 @@ class Zed < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/authzed/zed/releases/download/v0.22.0/zed_0.22.0_linux_arm64_gnu.tar.gz"
-        sha256 "defc600619d3a0cf4a4560e6b3850a9b4bf268e9cbc94ac3754bd80a19fd8139"
+        url "https://github.com/authzed/zed/releases/download/v0.23.0/zed_0.23.0_linux_arm64_gnu.tar.gz"
+        sha256 "e251e93b0ab97d4acf504910f995fd59922b0b94e3057e55fbc3bfbd31ad7828"
 
         def install
           if build.head?
